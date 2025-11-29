94f719150e79a35a1c92bb2583c8dcdb1ed5bf53
Clozerss_ <111479228+clozers@users.noreply.github.com>
2025-11-29 14:50:10 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | SPAI</title>
+    <title>Login | DDRI</title>
     <link rel="stylesheet"
         href="{{ asset('assets_auth/style.css') }}?v={{ filemtime(public_path('assets_auth/style.css')) }}">
 </head>
